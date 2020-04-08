edd1ce10d7f5fe25846c9e90744ec9379a9e468d
PilarCha <pchavez92@gmail.com>
2020-04-07 18:54:28 -0700

    Created controller for articles

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -4,5 +4,15 @@ RSpec.feature "Creating Articles" do
     scenario "A user creates a new article" do
       visit "/"
 
+      click_link "New Article"
+
+      fill_in "Title", with:  'New Article'
+      fill_in "Body", with:  "My culo is indeed huge"
+
+      click_button "Create Article"
+
+      expect(page).to have_content("Article has been created")
+      expect(page.current_path).to eq(articles_path)
+
     end
 end
